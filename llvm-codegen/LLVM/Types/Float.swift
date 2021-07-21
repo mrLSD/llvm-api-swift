12d1535948a2aa745d90f5a064dd5360a052c5cc
Evgeny Ukhanov <evgeny@aurora.dev>
2021-07-21 21:56:48 +0200

    Added Float types. Refactored TargetExtType

---
+++ llvm-codegen/LLVM/Types/Float.swift
@@ -0,0 +1,123 @@
+import CLLVM
+
+/// `FloatingType`  representations of a floating value of a particular
+/// bit width and semantics.
+public class FloatingType: TypeRef {
+    var llvm: LLVMTypeRef
+
+    /// Returns the context associated with this type.
+    public let context: Context?
+
+    /// Retrieves the underlying LLVM type object.
+    public var typeRef: LLVMTypeRef { llvm }
+
+    /// For parent classes init with predefined `FloatType` in global context
+    init(typeRef: LLVMTypeRef) {
+        llvm = typeRef
+        context = nil
+    }
+
+    /// For parent classes init with predefined `FloatType` in Context
+    init(typeRef: LLVMTypeRef, context: Context) {
+        llvm = typeRef
+        self.context = context
+    }
+}
+
+/// 16-bit floating point type
+public class HalfType: FloatingType {
+    /// Creates an instance of the `HalfType` type  in the global context.
+    public init() {
+        super.init(typeRef: LLVMHalfType())
+    }
+
+    /// Creates an instance of the `HalfType` type  in the Context.
+    public init(in context: Context) {
+        super.init(typeRef: LLVMHalfTypeInContext(context.contextRef))
+    }
+}
+
+/// 16-bit brain floating point type
+public class BFloatType: FloatingType {
+    /// Creates an instance of the `BFloatType` type  in the global context.
+    public init() {
+        super.init(typeRef: LLVMBFloatType())
+    }
+
+    /// Creates an instance of the `BFloatType` type  in the Context.
+    public init(in context: Context) {
+        super.init(typeRef: LLVMBFloatTypeInContext(context.contextRef))
+    }
+}
+
+
+/// 32-bit floating point type
+public class FloatType: FloatingType {
+    /// Creates an instance of the `FloatType` type  in the global context.
+    public init() {
+        super.init(typeRef: LLVMFloatType())
+    }
+
+    /// Creates an instance of the `FloatType` type  in the Context.
+    public init(in context: Context) {
+        super.init(typeRef: LLVMFloatTypeInContext(context.contextRef))
+    }
+}
+
+/// 64-bit floating point type
+public class DoubleType: FloatingType {
+    /// Creates an instance of the `DoubleType` type  in the global context.
+    public init() {
+        super.init(typeRef: LLVMDoubleType())
+    }
+
+    /// Creates an instance of the `DoubleType` type  in the Context.
+    public init(in context: Context) {
+        super.init(typeRef: LLVMDoubleTypeInContext(context.contextRef))
+    }
+}
+
+/// 80-bit floating point (X87) type
+public class X86FP80Type: FloatingType {
+    /// Creates an instance of the `x86FP80Type` type  in the global context.
+    public init() {
+        super.init(typeRef: LLVMX86FP80Type())
+    }
+
+    /// Creates an instance of the `x86FP80Type` type  in the Context.
+    public init(in context: Context) {
+        super.init(typeRef: LLVMX86FP80TypeInContext(context.contextRef))
+    }
+}
+
+/// 128-bit floating point (112-bit mantissa) type
+public class FP128Type: FloatingType {
+    /// Creates an instance of the `FP128Type` type  in the global context.
+    public init() {
+        super.init(typeRef: LLVMFP128Type())
+    }
+
+    /// Creates an instance of the `FP128Type` type  in the Context.
+    public init(in context: Context) {
+        super.init(typeRef: LLVMFP128TypeInContext(context.contextRef))
+    }
+}
+
+/// 128-bit floating point (two 64-bits)  type
+public class PPCFP128Type: FloatingType {
+    /// Creates an instance of the `PPCFP128Type` type  in the global context.
+    public init() {
+        super.init(typeRef: LLVMPPCFP128Type())
+    }
+
+    /// Creates an instance of the `PPCFP128Type` type  in the Context.
+    public init(in context: Context) {
+        super.init(typeRef: LLVMPPCFP128TypeInContext(context.contextRef))
+    }
+}
+
+extension FloatingType: Equatable {
+    public static func == (lhs: FloatingType, rhs: FloatingType) -> Bool {
+        return lhs.typeRef == rhs.typeRef
+    }
+}
--- llvm-codegen/LLVM/Types/Other/TargetExt.swift
+++ llvm-codegen/LLVM/Types/Other/TargetExt.swift
@@ -1,6 +1,7 @@
 import CLLVM
 
-public struct LLVMTargetExtType: TypeRef {
+///  Target extension type
+public struct TargetExtType: TypeRef {
     private let llvm: LLVMTypeRef
 
     /// Returns the context associated with this type.
@@ -9,27 +10,24 @@ public struct LLVMTargetExtType: TypeRef {
     /// Retrieves the underlying LLVM type object.
     public var typeRef: LLVMTypeRef { llvm }
 
-    /// Creates an instance of the `LLVMTargetExt` type  in the  context.
-    public init(in context: Context, name: String, typeParams: [LLVMTypeRef?], intParams: [UInt32]) {
-        var mutableTypeParams = typeParams
+    /// Creates an instance of the `TargetExt` type  in the  context.
+    public init(in context: Context, name: String, typeParams: [TypeRef], intParams: [UInt32]) {
+        var mutableTypeParams = typeParams.map { $0.typeRef as Optional }
         var mutableIntParams = intParams
-        llvm = name.withCString { cName in
-            mutableTypeParams.withUnsafeMutableBufferPointer { typeParamsBuffer in
-                mutableIntParams.withUnsafeMutableBufferPointer { intParamsBuffer in
-                    LLVMTargetExtTypeInContext(context.contextRef, cName,
-                                               typeParamsBuffer.baseAddress,
-                                               UInt32(typeParams.count),
-                                               intParamsBuffer.baseAddress,
-                                               UInt32(intParams.count))
-                }
-            }
-        }
+        let cName = name.withCString { cName in cName }
+        let ptrTypeParams = mutableTypeParams.withUnsafeMutableBufferPointer { typeParamsBuffer in typeParamsBuffer.baseAddress }
+        let ptrIntParams = mutableIntParams.withUnsafeMutableBufferPointer { intParamsBuffer in intParamsBuffer.baseAddress }
+        self.llvm = LLVMTargetExtTypeInContext(context.contextRef, cName,
+                                          ptrTypeParams,
+                                          UInt32(typeParams.count),
+                                          ptrIntParams,
+                                          UInt32(intParams.count))!
         self.context = context
     }
 }
 
-extension LLVMTargetExtType: Equatable {
-    public static func == (lhs: LLVMTargetExtType, rhs: LLVMTargetExtType) -> Bool {
+extension TargetExtType: Equatable {
+    public static func == (lhs: TargetExtType, rhs: TargetExtType) -> Bool {
         return lhs.typeRef == rhs.typeRef
     }
 }
