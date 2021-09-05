5d59072abff49f9926ba20dce220f54c5fce75d5
Evgeny Ukhanov <evgeny@aurora.dev>
2021-09-05 18:49:37 +0200

    Extend context api with all templated funcs

--- llvm-codegen/LLVM/Context.swift
+++ llvm-codegen/LLVM/Context.swift
@@ -86,34 +86,37 @@ public class Context: ContextRef {
         }
     }
 
-//=========
+    //===============================
+    //###############################
+    //===============================
+
     /// Get the diagnostic context of this context.
-    public func getDiagnosticContext(){
-// void *LLVMContextGetDiagnosticContext(LLVMContextRef C);
+    public func getDiagnosticContext() {
+        // void *LLVMContextGetDiagnosticContext(LLVMContextRef C);
     }
 
     /// Set the yield callback function for this context.
     public func setYieldCallback() {
-      //  void LLVMContextSetYieldCallback(LLVMContextRef C, LLVMYieldCallback Callback,
+        //  void LLVMContextSetYieldCallback(LLVMContextRef C, LLVMYieldCallback Callback,
     }
 
     /// Return a string representation of the DiagnosticInfo. Use
     /// LLVMDisposeMessage to free the string.
-    public func getDiagInfoDescription(){
+    public func getDiagInfoDescription() {
         // char *LLVMGetDiagInfoDescription(LLVMDiagnosticInfoRef DI);
     }
 
     ///  Return an enum LLVMDiagnosticSeverity.
     public func getDiagInfoSeverity() {
-// LLVMDiagnosticSeverity LLVMGetDiagInfoSeverity(LLVMDiagnosticInfoRef DI);
+        // LLVMDiagnosticSeverity LLVMGetDiagInfoSeverity(LLVMDiagnosticInfoRef DI);
     }
 
-    public func getMDKindIDInContext(){
-       // unsigned LLVMGetMDKindIDInContext(LLVMContextRef C, const char *Name,
+    public func getMDKindIDInContext() {
+        // unsigned LLVMGetMDKindIDInContext(LLVMContextRef C, const char *Name,
     }
 
-    public func getMDKindID(){
-    // unsigned LLVMGetMDKindID(const char *Name, unsigned SLen);
+    public func getMDKindID() {
+        // unsigned LLVMGetMDKindID(const char *Name, unsigned SLen);
     }
 
     /// Return an unique id given the name of a enum attribute,
@@ -125,85 +128,73 @@ public class Context: ContextRef {
     ///
     /// NB: Attribute names and/or id are subject to change without
     /// going through the C API deprecation cycle.
-    public func getEnumAttributeKindForName(){
+    public func getEnumAttributeKindForName() {
         // unsigned LLVMGetEnumAttributeKindForName(const char *Name, size_t SLen);
-
     }
 
-    public func getLastEnumAttributeKind (){
+    public func getLastEnumAttributeKind() {
         // unsigned LLVMGetLastEnumAttributeKind(void);
     }
 
-    public func
-    public func
-    public func
-    public func
-    public func
-    public func
-    public func
-    public func
-    public func
-    public func
-    public func
+    /// Create an enum attribute.
+    public func attributeRef() {
+        // LLVMAttributeRef LLVMCreateEnumAttribute(LLVMContextRef C, unsigned KindID, uint64_t Val);
+    }
 
-//========================================
-    /**
-     * Create an enum attribute.
-     */
-    LLVMAttributeRef LLVMCreateEnumAttribute(LLVMContextRef C, unsigned KindID,
-                                             uint64_t Val);
+    /// Get the unique id corresponding to the enum attribute passed as argument.
+    public func getEnumAttributeKind() {
+        // unsigned LLVMGetEnumAttributeKind(LLVMAttributeRef A);
+    }
 
-    /**
-     * Get the unique id corresponding to the enum attribute
-     * passed as argument.
-     */
-    unsigned LLVMGetEnumAttributeKind(LLVMAttributeRef A);
+    /// Get the enum attribute's value. 0 is returned if none exists.
+    public func getEnumAttributeValue() {
+        // uint64_t LLVMGetEnumAttributeValue(LLVMAttributeRef A);
+    }
 
-    /**
-     * Get the enum attribute's value. 0 is returned if none exists.
-     */
-    uint64_t LLVMGetEnumAttributeValue(LLVMAttributeRef A);
+    /// Create a type attribute
+    public func createTypeAttribute() {
+        // LLVMAttributeRef LLVMCreateTypeAttribute(LLVMContextRef C, unsigned KindID, LLVMTypeRef type_ref);
+    }
 
-    /**
-     * Create a type attribute
-     */
-    LLVMAttributeRef LLVMCreateTypeAttribute(LLVMContextRef C, unsigned KindID,
-                                             LLVMTypeRef type_ref);
+    /// Get the type attribute's value.
+    public func getTypeAttributeValue() {
+        // LLVMTypeRef LLVMGetTypeAttributeValue(LLVMAttributeRef A);
+    }
 
-    /**
-     * Get the type attribute's value.
-     */
-    LLVMTypeRef LLVMGetTypeAttributeValue(LLVMAttributeRef A);
+    /// Create a string attribute.
+    public func createStringAttribute() {
+        // TODO: LLVMAttributeRef LLVMCreateStringAttribute(LLVMContextRef C,
+    }
 
-    /**
-     * Create a string attribute.
-     */
-    LLVMAttributeRef LLVMCreateStringAttribute(LLVMContextRef C,
-                                               const char *K, unsigned KLength,
-                                               const char *V, unsigned VLength);
+    /// Get the string attribute's kind.
+    public func getStringAttributeKind() {
+        // const char *LLVMGetStringAttributeKind(LLVMAttributeRef A, unsigned *Length);
+    }
 
-    /**
-     * Get the string attribute's kind.
-     */
-    const char *LLVMGetStringAttributeKind(LLVMAttributeRef A, unsigned *Length);
+    /// Get the string attribute's value.
+    public func getStringAttributeValue() {
+        // const char *LLVMGetStringAttributeValue(LLVMAttributeRef A, unsigned *Length);
+    }
 
-    /**
-     * Get the string attribute's value.
-     */
-    const char *LLVMGetStringAttributeValue(LLVMAttributeRef A, unsigned *Length);
+    /// Check for the  types of attributes.
+    public func isEnumAttribute() {
+        // LLVMBool LLVMIsEnumAttribute(LLVMAttributeRef A);
+    }
 
-    /**
-     * Check for the different types of attributes.
-     */
-    LLVMBool LLVMIsEnumAttribute(LLVMAttributeRef A);
-    LLVMBool LLVMIsStringAttribute(LLVMAttributeRef A);
-    LLVMBool LLVMIsTypeAttribute(LLVMAttributeRef A);
+    /// Check for the  types of attributes.
+    public func isStringAttribute() {
+        // LLVMBool LLVMIsStringAttribute(LLVMAttributeRef A);
+    }
 
-    /**
-     * Obtain a Type from a context by its registered name.
-     */
-    LLVMTypeRef LLVMGetTypeByName2(LLVMContextRef C, const char *Name);
+    /// Check for the  types of attributes.
+    public func isTypeAttribute() {
+        // LLVMBool LLVMIsTypeAttribute(LLVMAttributeRef A);
+    }
 
+    /// Obtain a Type from a context by its registered name.
+    public func getTypeByName2() {
+        // LLVMTypeRef LLVMGetTypeByName2(LLVMContextRef C, const char *Name);
+    }
 
     /// Deinitialize this value and dispose of its resources.
     ///
